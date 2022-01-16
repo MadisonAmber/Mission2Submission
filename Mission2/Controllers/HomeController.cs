ff7c288ae8cfae93e1ab061d78c7e1ef019ff26f
Madison Clark <madamber4@gmail.com>
2022-01-15 18:17:37 -0700

    init mission 3

---
+++ Mission2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult GradeCalc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc(GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission2/Models/GradeCalcModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Models
+{
+    public class GradeCalcModel
+    {
+        //Make sure each input is entered
+        [Required]
+        //Make sure each input can only be a number between 0 and 100
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        public int Assignment { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        public int GroupProject { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        public int Quiz { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        public int Exam { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100.")]
+        public int Intex { get; set; }
+    }
+}
